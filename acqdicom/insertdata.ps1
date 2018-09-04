91c5dc69c24f72cf35d0036e7d08adf079d1902f
Riccardo Rolla <riccardo.rolla@gmail.com>
2018-09-04 10:06:01 +0200

    update

--- acqdicom/insertdata.ps1
+++ acqdicom/insertdata.ps1
@@ -1,3 +1,4 @@
+
 $conf = Read-Properties $PSScriptRoot\inventory.properties
 
 $DBConn = New-Object System.Data.Odbc.OdbcConnection
@@ -5,31 +6,25 @@ $DBConn.ConnectionString = $conf.odbc
 
 $conf.DB = $DBConn
 $DBCmd = $DBConn.CreateCommand()
- 
-[void]$DBCmd.Parameters.Add("@CallingAE", [System.Data.Odbc.OdbcType]::string)
-
 [void]$DBCmd.Parameters.Add("@SUID",  [System.Data.Odbc.OdbcType]::varchar, 100)
 [void]$DBCmd.Parameters.Add("@SeriesUID",  [System.Data.Odbc.OdbcType]::varchar, 100)
 [void]$DBCmd.Parameters.Add("@SopUID", 	 [System.Data.Odbc.OdbcType]::varchar, 100)
 [void]$DBCmd.Parameters.Add("@header", 	 [System.Data.Odbc.OdbcType]::string)
 [void]$DBCmd.Parameters.Add("@report", 	 [System.Data.Odbc.OdbcType]::string)
-$DBCmd.CommandText = "INSERT INTO dicomdata (callingae,suid,seriesuid,sopuid,header,report) VALUES (?,?,?,?,?::JSONB,?::JSONB)"
+$DBCmd.CommandText = "INSERT INTO dicomdata (suid,seriesuid,sopuid,header,report) VALUES (?,?,?,?::JSONB,?::JSONB)"
 $DBCmd.Connection.Open()
 $conf.DBCmd=$DBCmd
 start-dicomserver -Port $conf.port -AET $conf.aet  -Environment $conf  -onCStoreRequest {
 	param($request,$file,$association,$env) 
 
 	  import-module D:\workspace\acqdicom\getsr.psm1
- 
+
 		$header = $file | read-dicom 
 		$header.Modality
-	    	$header.Remove('PixelData')
+	    $header.Remove('PixelData')
 		$headerJSON  = $header   | ConvertTo-JSON -depth 5
-	
+
 	 	$SRJSON = GetConceptValue $header.ContentSequence | ConvertTo-JSON -depth 10
- 
-		$env.DBCmd.Parameters["@CallingAE"].Value= $association.CallingAE
- 
 		$env.DBCmd.Parameters["@SUID"].Value = $header.StudyInstanceUID
 		$env.DBCmd.Parameters["@SeriesUID"].Value = $header.SeriesInstanceUID
 		$env.DBCmd.Parameters["@SopUID"].Value = $header.SOPInstanceUID
@@ -40,24 +35,22 @@ start-dicomserver -Port $conf.port -AET $conf.aet  -Environment $conf  -onCStore
 		[Dicom.Network.DicomStatus]::success
 }
 
-$startdate=(get-date).AddDays(-7)
-$list_rules = import-csv $PSScriptRoot\retrieve_rules.csv
- 
+$startdate=(get-date).AddDays(-2)
+#si cicla fino a che non si arriva ad oggi
 while ($startdate -lt (get-date)) {
-	 
+		#stampo la data iniziale 
 		Write-Host $startdate
-		 
+		#creo la datatime finale  aggiungendo 30 minuti alla data iniziale
 		$enddate=$startdate.AddMinutes(30)
-		foreach ($rules in $list_rules) {
-			Write-Host search-dicom -SopClassProvider $rules.SopClassProvider -AET $conf.aet -startdate $startdate -enddate $enddate -Modality $rules.Modality
-	 
-			$studies=search-dicom -SopClassProvider $rules.SopClassProvider -AET $conf.aet -startdate $startdate -enddate $enddate   -Modality $rules.Modality
-	 
-			foreach ($study in $studies) {
- 
-					move-dicom -Study -SopClassProvider $rules.SopClassProvider -AET $conf.aet -moveTo  $conf.aet -StudyInstanceUID $study.StudyInstanceUID 
-				}
-		}
+		
+		Write-Host search-dicom -SopClassProvider $conf.pacs -AET $conf.aet -startdate $startdate -enddate $enddate
+		#ottengo gli studi fatti tra la datatime iniziale e datatime finale
+		$studies=search-dicom -SopClassProvider $conf.pacs -AET $conf.aet -startdate $startdate -enddate $enddate   
+		#per ogni studio trovato
+		foreach ($study in $studies) {
+				#faccio la move verso il dicomserver instanziato dallo script
+				move-dicom -Study -SopClassProvider  $conf.pacs  -AET $conf.aet -moveTo  $conf.aet -StudyInstanceUID $study.StudyInstanceUID 
+			}
 		
 		$startdate=$enddate
 } 
